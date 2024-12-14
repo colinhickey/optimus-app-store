0489c4cae4aaf11932c38cbc07dd84477fa633d3
colinhickey <colinhickey@mac.com>
2024-12-14 17:34:19 -0500

    neuralink icon

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const robotImage = document.getElementById("robot-image");
   const scrollContainer = document.getElementById("scroll-container");
   const modal = document.getElementById('modal');
-const modalContent = document.getElementById('modal-content');
-const closeModal = document.getElementById('close-modal');
-const modalImage = document.getElementById('modal-image');
-const modalName = document.getElementById('modal-name');
-const modalSubtitle = document.getElementById('modal-subtitle');
-const modalDescription = document.getElementById('modal-description');
-const modalCategory = document.getElementById('modal-category');
-const modalRating = document.getElementById('modal-rating');
-const modalPrice = document.getElementById('modal-price');
-const modalAccessories = document.getElementById('modal-accessories');
+  const modalContent = document.getElementById('modal-content');
+  const closeModal = document.getElementById('close-modal');
+  const closeModalMobile = document.getElementById('close-modal-mobile');
+  const modalImage = document.getElementById('modal-image');
+  const modalName = document.getElementById('modal-name');
+  const modalSubtitle = document.getElementById('modal-subtitle');
+  const modalDescription = document.getElementById('modal-description');
+  const modalCategory = document.getElementById('modal-category');
+  const modalRating = document.getElementById('modal-rating');
+  const modalPrice = document.getElementById('modal-price');
+  const modalAccessories = document.getElementById('modal-accessories');
   let animationComplete = false;
 
   function createGlowingAnimation(top, left) {
@@ -92,12 +93,6 @@ const modalAccessories = document.getElementById('modal-accessories');
     // Show or hide elements based on scroll progress
     if (scrollFraction >= 1 && !animationComplete) {
       startAnimation();
-      // animationComplete = true;
-      // welcomeSection.remove(); // Remove the welcome-section from the DOM
-      // appList.classList.remove("hidden");
-      // header.classList.remove("hidden");
-      // scrollContainer.style.height = "100vh"; // Adjust the height of the scroll-container
-      // window.scrollTo(0, 0); // Scroll back to the top
     } else if (!animationComplete) {
       welcomeSection.style.display = "flex";
       appList.classList.add("hidden");
@@ -124,6 +119,8 @@ const modalAccessories = document.getElementById('modal-accessories');
   window.addEventListener("scroll", updateAnimationProgress);
   welcomeSection.addEventListener('click', startAnimation);
 
+  const neuralinkSVG = `<svg class="neuralinkIcon" width="56" height="35" viewBox="0 0 56 35" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="#e6e5e5" fill-rule="evenodd" clip-rule="evenodd" d="M30.8777 22.4886H0.705078L22.5011 2.28693C24.3585 0.565492 27.0293 -0.000275522 29.47 0.809903C31.9102 1.62058 33.6439 3.6498 33.994 6.10498L37.476 30.5019C37.5803 31.2306 38.0767 31.8049 38.8036 32.0367C39.5305 32.2696 40.2868 32.0946 40.8255 31.572L50.1712 22.4886H39.4676L39.1096 20.2688H55.7051L42.4717 33.131C41.6499 33.9302 40.5593 34.3566 39.4367 34.3566C38.9823 34.3566 38.5221 34.2862 38.073 34.1429C36.5143 33.6455 35.4074 32.3656 35.1842 30.8031L31.7021 6.40672C31.4673 4.75921 30.3499 3.45166 28.7127 2.90802C27.0759 2.36337 25.3548 2.72899 24.108 3.88365L6.42923 20.2688H30.5522L30.8777 22.4886Z"></path></svg>`
+
   fetch("data.json")
     .then((response) => response.json())
     .then((data) => {
@@ -137,6 +134,7 @@ const modalAccessories = document.getElementById('modal-accessories');
                     <p class="subtitle">${app.subtitle}</p>
                     <p class="category">${app.category}</p>      
                     <p class="rating">${app.rating} ⭐</p>
+                    ${app.neuralink ? neuralinkSVG : ''}
                 `;
         appCard.addEventListener('click', () => {openModal(app)});
         appList.appendChild(appCard);
@@ -146,6 +144,10 @@ const modalAccessories = document.getElementById('modal-accessories');
     closeModal.addEventListener('click', () => {
         modal.classList.add('hidden');
     });
+
+    closeModalMobile.addEventListener('click', () => {
+      modal.classList.add('hidden');
+  });
 
   // Create multiple glowing animations
   createMultipleGlowingAnimations(6); // Adjust the number of animations as needed
